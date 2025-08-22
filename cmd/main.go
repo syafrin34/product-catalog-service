0fb1a44e092fdfa1e17fe8c06cc244acb46cb596
syafrin ibrahim <syafrinibrahim12@gmail.com>
2025-08-22 09:02:43 +0800

    add error logger

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"product-catalog-service/internal/api"
+	"product-catalog-service/internal/repository"
 	"product-catalog-service/internal/service"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 
 	// initialize product service
-
-	productService := service.NewProductService()
+	productRepo := repository.NewProductRepository()
+	productService := service.NewProductService(*productRepo)
 	productHandler := api.NewProductHandler(*productService)
 
 	// initialize echo
 	e := echo.New()
+
+	// middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(echojwt.JWT([]byte("secret")))
 
 	// routes
 	e.GET("/products/:id/stock", productHandler.GetProductStock)
--- internal/service/product_service.go
+++ internal/service/product_service.go
@@ -1,24 +1,68 @@
 package service
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"product-catalog-service/internal/repository"
 
+	"github.com/rs/zerolog"
+)
+
+var logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
 type ProductService struct {
+	productRepo repository.ProductRepository
 }
 
-func NewProductService() *ProductService {
-	return &ProductService{}
+func NewProductService(pRepo repository.ProductRepository) *ProductService {
+	return &ProductService{
+		productRepo: pRepo,
+	}
 }
 
 func (p *ProductService) GetProductStock(productID int) (int, error) {
-	stock := 50
-	return stock, nil
+	product, err := p.productRepo.GetProductByID(productID)
+	if err != nil {
+		logger.Error().Err(err).Msgf("Error getting product by ID", productID)
+		return  0, err
+	}
+	return  product.Stock, nil
 }
+
+// reserve stock for an order
 func (p *ProductService) ReserveProductStock(productID int, quantity int) error {
-	fmt.Printf("Reserved %d units of product %d\n", quantity, productID)
-	return nil
+	
+	product, err := p.productRepo.GetProductByID(productID)
+	if err != nil {
+		logger.Error().Err(err).Msgf("Error getting product by ID", productID)
+		return  err
+	}
+	if product.Stock < quantity {
+		logger.Warn().Msgf("Product %d out of stock", productID)
+		return fmt.Errorf("product out of stock")
+	}
+
+	product.Stock -= quantity
+	_, err = p.productRepo.UpdateProduct(product)
+	if err != nil {
+		logger.Error().Err(err).Msgf("Error updating product %d", productID)
+		return  err
+	}
+	return  nil
 }
+
+// release product reserved stock when an order is cancelled
 func (p *ProductService) ReleaseProductStock(productID int, quantity int) error {
-	fmt.Printf("Release %d units of product %d\n", quantity, productID)
-	return nil
+	product, err := p.productRepo.GetProductByID(productID)
+	if err != nil {
+		logger.Error().Err(err).Msgf("Error getting product by ID", productID)
+		return  err
+	}
+	product.Stock += quantity
+	_, err = p.productRepo.UpdateProduct(product)
+	if err != nil {
+		logger.Error().Err(err).Msgf("Error getting product by ID", productID)
+		return  err
+	}
+	return  nil
 
 }
